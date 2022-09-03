3686dc1ef48b8a880b1659836a6d493b30fa3e55
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-09-03 23:26:13 +0530

    fix: layout updated to min height screen

--- src/common/Layout/index.tsx
+++ src/common/Layout/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="dark:text-white dark:bg-black text-black bg-white transition-all duration-500">
+    <div className="dark:text-white dark:bg-black text-black bg-white transition-all duration-500 min-h-screen">
       {children}
     </div>
   );
